846898b0dd3a334b10aba4244245cfae4a23db7c
William Hart <wehart@sandia.gov>
2019-12-04 12:52:28 -0700

    Two changes
    1. Updating test scripts for gurobi, generating a CSV summary file
    
    2. Adding a preliminary OPF problem.

--- lqcp/testgurobi.sh
+++ lqcp/testgurobi.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
 echo "500"
-time ./lqcp 500
-
+(time -p ./lqcp  500) > testgurobi.500.out 2>&1
 echo "1000"
-time ./lqcp 1000
-
+(time -p ./lqcp 1000) > testgurobi.1000.out 2>&1
 echo "1500"
-time ./lqcp 1500
-
+(time -p ./lqcp 1500) > testgurobi.1500.out 2>&1
 echo "2000"
-time ./lqcp 2000
+(time -p ./lqcp 2000) > testgurobi.2000.out 2>&1
+
+
+echo "size,gurobi" > gurobi.csv
+for i in 500 1000 1500 2000
+do
+    tmp=`grep user "testgurobi.${i}.out" | awk '{print $2}'`
+    if [ "$tmp" == "" ]; then
+        tmp="0"
+    fi;
+    echo "$i,$tmp" >> gurobi.csv
+done
+
--- pmedian/testgurobi.sh
+++ pmedian/testgurobi.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
 echo "160 1"
-time ./pmedian  160 1
-
+(time -p ./pmedian  160 1) > testgurobi.160.out 2>&1
 echo "320 1"
-time ./pmedian  320 1
-
+(time -p ./pmedian  320 1) > testgurobi.320.out 2>&1
 echo "640 1"
-time ./pmedian  640 1
-
+(time -p ./pmedian  640 1) > testgurobi.640.out 2>&1
 echo "1280 1"
-time ./pmedian 1280 1
+(time -p ./pmedian 1280 1) > testgurobi.1280.out 2>&1
+
+
+echo "size,gurobi" > gurobi.csv
+for i in 160 320 640 1280
+do
+    tmp=`grep user "testgurobi.${i}.out" | awk '{print $2}'`
+    if [ "$tmp" == "" ]; then
+        tmp="0"
+    fi;
+    echo "$i,$tmp" >> gurobi.csv
+done
+
