2d89eea897f2b0d73c9f19987a9448afa75edf0e
William Hart <wehart@sandia.gov>
2019-12-08 09:28:54 -0700

    Adding timeout logic

--- run_problem.sh
+++ run_problem.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# NOTE: We want to run final tests with a timeout value of 10m
+# but for development purposes we may use a shorter limit.
+if [ "$TEST_TIMEOUT" == "" ]; then
+    TEST_TIMEOUT="10m"
+fi
+
 function run_problem ()
 {
 local name=$1
@@ -8,16 +14,25 @@ local command=$1
 shift
 local size=("$@")
 
-echo "size,${name}" > ${name}.csv
+echo "size,${name},status" > ${name}.csv
 for i in "${size[@]}"
 do
     echo "NAME $name  SIZE $i"
-    time -p sh -c "${command//'$i'/$i}" > test${name}.${i}.out 2>&1
+    timeout $TEST_TIMEOUT time -p sh -c "${command//'$i'/$i}" > test${name}.${i}.out 2>&1
+    retval=$?
+
+    status="ok"
+    if [ "$retval" -eq "124" ]; then
+        status="timeout"
+    elif [ "$retval" -ne "0" ]; then
+        status="error"
+    fi
+    echo "Status: ${status} ($retval)"
 
     tmp=`grep user "test${name}.${i}.out" | awk '{print $2}'`
     if [ "$tmp" == "" ]; then
         tmp="0"
     fi;
-    echo "$i,$tmp" >> ${name}.csv
+    echo "$i,$tmp,$status" >> ${name}.csv
 done
 }
