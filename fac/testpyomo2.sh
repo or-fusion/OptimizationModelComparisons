159df23fb1bd19949e20ae5e6a996236af61e29c
William Hart <wehart@sandia.gov>
2019-12-10 20:18:36 -0700

    Adding pyomo-native solver tests

---
+++ fac/testpyomo2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+    suffix=""
+else
+    suffix="_$1"
+fi
+
+source ../run_problem.sh
+
+size=(25 50 75 100)
+name="pyomo2"
+command="python ./facility_${name}.py \$i 1"
+
+run_problem ${name}${suffix} "$command" ${size[@]}
